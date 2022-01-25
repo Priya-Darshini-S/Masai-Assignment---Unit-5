b9f10e27c0a242fd147582bdcaea604ea0d13d97
Priya_darshini_s <priya06200@gmail.com>
2022-01-25 14:47:57 +0530

    Update App.jsx

--- redux_react_hooks/src/App.jsx
+++ redux_react_hooks/src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import TodoInput from "./components/TodoInput";
-import TodoList from "./components/TodoList";
 import { AllRoutes } from "./Routes/AllRoutes";
 
 function App() {
